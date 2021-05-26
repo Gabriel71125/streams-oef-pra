b56af04e2e9cb4c2affe9f6b2e443020819dd207
Gabriël Goemaere <71774559+Gabriel71125@users.noreply.github.com>
2021-05-26 09:11:16 +0200

    Initial commit

---
+++ pra.streams.cursus01.WPF/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace pra.streams.cursus01.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        string foutMelding;
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+
+        private void btnWriteTextFile_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnReadTextFile_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
